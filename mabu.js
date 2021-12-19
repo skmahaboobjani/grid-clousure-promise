d46bec44d8472e86ecd14e80288ce84cd33d3de8
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-12-19 09:43:56 +0530

    Add files via upload

---
+++ mabu.js
@@ -0,0 +1,32 @@
+// clousure example
+
+// function fn()
+// {
+//     var x=10
+//     function fn1()
+//     {
+//         console.log(x)
+//     }
+//     fn1()
+// }
+// fn()
+// promise example
+
+// let mabu=new Promise(function(resolve,reject){
+//     var x=10
+//     if(x==10)
+//     {
+//         console.log("ok")
+//     }
+//     else{
+//         console.log("error")
+//     }
+// })
+// mabu.then(function (val){
+//     console.log(val)
+    
+// },
+// function(val2){
+//     console.log(val2)
+
+// })
